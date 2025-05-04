b80c1cab9fa0562072bcfc0be45ca10e1fcfa3dd
Lydia <lida_zhuikova@mail.ru>
2025-05-04 20:24:29 +0400

    feat: add new function

--- app/src/main/java/com/example/mycityapp/data/datasource/RecommendationDataSource.kt
+++ app/src/main/java/com/example/mycityapp/data/datasource/RecommendationDataSource.kt
@@ -22,25 +22,57 @@ class RecommendationDataSource {
             ),
             Recommendation(
                 id = 2,
-                nameResId = R.string.category_cafe_1,
+                nameResId = R.string.category_cafe_2,
                 descriptionResId = R.string.category_cafe_2_description,
                 imageResId = R.drawable.category_cafe_2
             ),
             Recommendation(
                 id = 3,
-                nameResId = R.string.category_cafe_1,
+                nameResId = R.string.category_cafe_3,
                 descriptionResId = R.string.category_cafe_3_description,
                 imageResId = R.drawable.category_cafe_3
             ),
             Recommendation(
                 id = 4,
-                nameResId = R.string.category_cafe_1,
+                nameResId = R.string.category_cafe_4,
                 descriptionResId = R.string.category_cafe_4_description,
                 imageResId = R.drawable.category_cafe_4
             ),
             Recommendation(
                 id = 5,
+                nameResId = R.string.category_cafe_5,
+                descriptionResId = R.string.category_cafe_5_description,
+                imageResId = R.drawable.category_cafe_5
+            )
+        ),
+        R.string.category_restaurants to listOf(
+            Recommendation(
+                id = 1,
                 nameResId = R.string.category_cafe_1,
+                descriptionResId = R.string.category_cafe_1_description,
+                imageResId = R.drawable.category_cafe_1
+            ),
+            Recommendation(
+                id = 2,
+                nameResId = R.string.category_cafe_2,
+                descriptionResId = R.string.category_cafe_2_description,
+                imageResId = R.drawable.category_cafe_2
+            ),
+            Recommendation(
+                id = 3,
+                nameResId = R.string.category_cafe_3,
+                descriptionResId = R.string.category_cafe_3_description,
+                imageResId = R.drawable.category_cafe_3
+            ),
+            Recommendation(
+                id = 4,
+                nameResId = R.string.category_cafe_4,
+                descriptionResId = R.string.category_cafe_4_description,
+                imageResId = R.drawable.category_cafe_4
+            ),
+            Recommendation(
+                id = 5,
+                nameResId = R.string.category_cafe_5,
                 descriptionResId = R.string.category_cafe_5_description,
                 imageResId = R.drawable.category_cafe_5
             )
@@ -51,5 +83,9 @@ class RecommendationDataSource {
 
     fun getRecommendations(categoryId: Int): List<Recommendation> {
         return recommendations[categoryId] ?: emptyList()
+    }
+
+    fun getRecommendationById(categoryId: Int, recommendationId: Int): Recommendation? {
+        return recommendations[categoryId]?.find { it.id == recommendationId }
     }
 }
