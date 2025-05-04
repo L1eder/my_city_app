330211cbd5752aeb196d2961a73c5e407c0f511e
Lydia <lida_zhuikova@mail.ru>
2025-05-04 20:31:00 +0400

    feat: changed MainActivity, now you can view recommendations and navigate through screens

--- app/src/main/java/com/example/mycityapp/MainActivity.kt
+++ app/src/main/java/com/example/mycityapp/MainActivity.kt
@@ -3,45 +3,46 @@ package com.example.mycityapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mycityapp.ui.theme.MyCityAppTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mycityapp.data.datasource.RecommendationDataSource
+import com.example.mycityapp.data.repository.RecommendationRepository
+import com.example.mycityapp.presentation.screen.CategoryScreen
+import com.example.mycityapp.presentation.screen.DetailsScreen
+import com.example.mycityapp.presentation.screen.RecommendationsScreen
+import com.example.mycityapp.presentation.viewmodel.CategoryViewModel
+import com.example.mycityapp.presentation.viewmodel.DetailsViewModel
+import com.example.mycityapp.presentation.viewmodel.RecommendationsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyCityAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            val repository = RecommendationRepository(RecommendationDataSource())
+            val categoryViewModel = CategoryViewModel(repository)
+            val recommendationsViewModel = RecommendationsViewModel(repository)
+
+            NavHost(navController = navController, startDestination = "categories") {
+                composable("categories") {
+                    CategoryScreen(navController, categoryViewModel.categories)
+                }
+                composable("recommendations/{categoryId}") { backStackEntry ->
+                    val categoryId = backStackEntry.arguments?.getString("categoryId")?.toIntOrNull()
+                    recommendationsViewModel.loadRecommendations(categoryId ?: 0)
+                    RecommendationsScreen(navController, recommendationsViewModel, categoryId ?: 0)
+                }
+                composable("details/{categoryId}/{recommendationId}") { backStackEntry ->
+                    val categoryId = backStackEntry.arguments?.getString("categoryId")?.toIntOrNull()
+                    val recommendationId = backStackEntry.arguments?.getString("recommendationId")?.toIntOrNull()
+                    val detailsViewModel = DetailsViewModel(repository)
+                    if (categoryId != null && recommendationId != null) {
+                        detailsViewModel.loadRecommendation(categoryId, recommendationId)
+                    }
+                    DetailsScreen(detailsViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCityAppTheme {
-        Greeting("Android")
     }
 }
