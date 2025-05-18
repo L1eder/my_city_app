21f9efbbf8e81174b8e3963e63f6e0432449e31f
Lydia <lida_zhuikova@mail.ru>
2025-05-18 11:46:16 +0400

    feat: add new .kt file with routes

---
+++ app/src/main/java/com/example/mycityapp/navigation/Routes.kt
@@ -0,0 +1,7 @@
+package com.example.mycityapp.navigation
+
+object Routes {
+    const val CATEGORIES = "categories"
+    const val RECOMMENDATIONS = "recommendations/{categoryId}"
+    const val DETAILS = "details/{categoryId}/{recommendationId}"
+}
